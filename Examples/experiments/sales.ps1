44635e678825a5774f61f1c0052369eff1d2c359
dfinke <finked@hotmail.com>
2022-06-18 15:14:43 -0400

    tweak

--- Examples/experiments/sales.ps1
+++ Examples/experiments/sales.ps1
@@ -1,4 +1,7 @@
 Import-Module $PSScriptRoot\..\..\PowerShellPivot.psd1 -force
 
 #import-csv sales.csv | Format-Table
-import-csv sales.csv | Get-Subtotal region
+#Import-Csv $PSScriptRoot\sales.csv | Get-Subtotal 
+#import-csv $PSScriptRoot\sales.csv | ConvertTo-CrossTab
+# Import-Csv $PSScriptRoot\sales.csv | ConvertTo-CrossTab Region $null -ValueName Sales
+Import-Csv $PSScriptRoot\sales.csv | New-PSPivotTable #Region -PrettyPrint
